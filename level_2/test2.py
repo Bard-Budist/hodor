2fef3bcbbee9dbf810175a29e2198cc20f09184a
GiovanniAyala <op7puro@live.com>
2020-01-07 23:36:39 -0500

    Add header check

--- level_2/test2.py
+++ level_2/test2.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import requests
 
-url = "http://158.69.76.135/level2.php"
+url = "http://158.69.76.135/level1.php"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'}
 
 with requests.session() as client:
-	user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"
+
 	for i in range(1):
 		key = client.get(url)
- 		key_dic = key.cookies.get_dict()
+		key_dic = key.cookies.get_dict()
 		data_post = {
-			"id": 20019,
+			"id": 1,
 			"holdthedoor": "Submit",
 			"key": key_dic['HoldTheDoor']
 		}
-		headers = {'User-Agent': user_agent}
-		client.post(url, data_post, headers)
+		client.post(url, data_post, headers=headers)
