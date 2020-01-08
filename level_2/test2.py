c3760e881e67534a5489173f60036c14044e2bf1
GiovanniAyala <op7puro@live.com>
2020-01-08 12:42:35 -0500

    Fix task 01

--- level_2/test2.py
+++ level_2/test2.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import requests
-
 url = "http://158.69.76.135/level2.php"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0', 'Referer': 'http://158.69.76.135/level2.php'}
 
 with requests.session() as client:
-	user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"
-	for i in range(1):
-		key = client.get(url)
- 		key_dic = key.cookies.get_dict()
-		data_post = {
-			"id": 20019,
+    for i in range(10):
+        key = client.get(url)
+        key_dic = key.cookies.get_dict()
+        cookies = key.cookies['HoldTheDoor']
+        data_post = {
+			"id": 1992221,
 			"holdthedoor": "Submit",
 			"key": key_dic['HoldTheDoor']
 		}
-		headers = {'User-Agent': user_agent}
-		client.post(url, data_post, headers)
+        client.post(url, data=data_post, cookies={'HoldTheDoor': cookies}, headers=headers)s)
