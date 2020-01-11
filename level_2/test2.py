81a08014984d6d8a6bb5829054331a771a3837d4
GiovanniAyala <op7puro@live.com>
2020-01-10 19:50:44 -0500

    Add folder

--- level_2/test2.py
+++ level_2/test2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import requests
 
 url = "http://158.69.76.135/level2.php"
--- level_3/test3.py
+++ level_3/test3.py
@@ -1,31 +1,13 @@
 #!/usr/bin/python3
-import requests
-import os
-from requests import Request, Session
-#from parse_captcha import *
+from Screenshot import Screenshot_Clipping
+from selenium import webdriver
 
-strem = None
-verify = False
-proxies = None
-cert = None
-timeout = None
-url = "http://158.69.76.135/captcha.php"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0', 'Referer': 'http://158.69.76.135/level2.php'}
+image = Screenshot_Clipping.Screenshot()
+driver = webdriver.Firefox()
+url = "http://158.69.76.135/level3.php"
+driver.get(url)
+img_url=image.full_Screenshot(driver, save_path=r'.', image_name='Myimage.png')
+print(img_url)
+driver.close()
 
-with requests.session() as client:
-    for i in range(5):
-       #client.get(url)
-       # key_dic = client.cookies.get_dict()
-        key = Request('GET',url, cookies= client.cookies.get_dict())
-        prepped = client.prepare_request(key)
-        resp = client.send(prepped)
-        print(resp.cookies)
-        print("-------------------")
-        print(client.cookies)
-        data_post = {
-			"id": 1,
-			"holdthedoor": "Submit",
-			#"key": key_dic['HoldTheDoor']
-                        #"captcha": f.read()
-		}
-        #client.post(url, data=data_post, cookies={'HoldTheDoor': cookies}, headers=headers)
+driver.quit()
