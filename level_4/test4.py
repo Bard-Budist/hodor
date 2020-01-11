beb0322697620635e29349d97c64e47684c0bf8f
GiovanniAyala <op7puro@live.com>
2020-01-11 01:12:45 -0500

    Finally function

--- level_4/test4.py
+++ level_4/test4.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
+import os
 
 def getProxies(url):
     listProxies = []
     lenCheck = 0
     index = requests.get(url)
     soup = BeautifulSoup(index.content, 'html.parser')
-    for i in range(200, 300, 1):
+    for i in range(250, 300, 1):
         results = soup.find_all('tr')[i]
         for x in results:
             x = str(x).replace("<td>", "")
@@ -22,18 +23,17 @@ url = "http://158.69.76.135/level4.php"
 urlProxies = "https://free-proxy-list.net/index.html"
 
 proxies = getProxies(urlProxies)
-print(len(proxies))
-
-#client = requests.session()
-#for i in range(1):
-#    proxi = {'http': proxies[i], 'https': proxies[i + 1]}
-#    key = client.get(url,proxies = proxi)
-#    key_dic = key.cookies.get_dict()
-#    data_post = {
-#		'id': 1,
-#		'holdthedoor': "Submit",
-#		'key': key_dic['HoldTheDoor']
-#	}
-#client.post(url, data_post)
-
-
+with requests.session() as client:
+    for i in range(1):
+        os.system("unset HTTP_PROXY")
+        key = client.get(url)
+        key_dic = key.cookies.get_dict()
+        cookies = key.cookies['HoldTheDoor']
+        data_post = {
+			"id": 1,
+			"holdthedoor": "Submit",
+			"key": key_dic['HoldTheDoor']
+		}
+        os.environ["HTTP_PROXY"] = "http://" + proxies[4] + ":" + proxies[5]
+        #os.system("export HTTP_PROXY=" + '"' +"http://" + proxies[0] + ":" + proxies[1] + '"')
+        print(client.post(url, data=data_post))
