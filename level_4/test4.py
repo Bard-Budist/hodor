dce644839d5070bed935ee5e91bda40a88d9721f
GiovanniAyala <op7puro@live.com>
2020-01-12 02:09:40 -0500

    Casi lo termino :D

--- level_4/test4.py
+++ level_4/test4.py
@@ -4,21 +4,41 @@ from bs4 import BeautifulSoup
 import os
 import sys
 import urllib3
+import random
 
 def getProxies(url):
     listProxies = []
+    proxiReturn = []
     lenCheck = 0
     index = requests.get(url)
     soup = BeautifulSoup(index.content, 'html.parser')
-    for i in range(150, 300, 1):
+    for i in range(1, 300, 1):
         results = soup.find_all('tr')[i]
         for x in results:
             x = str(x).replace("<td>", "")
             firtsLetter = x.replace("</td>", "")
             if (firtsLetter[0] >= '0' and firtsLetter[0] <= '9'):
                 listProxies.append(firtsLetter)
-    print(listProxies)
-    return listProxies
+
+    print("Getting proxies...")
+    ran = random.randint(0, 300)
+    if (len(listProxies[ran]) > 8):
+        proxiReturn.append(str(listProxies[ran]))
+        proxiReturn.append(str(listProxies[ran + 1]))
+    else:
+        proxiReturn.append(str(listProxies[ran - 1]))
+        proxiReturn.append(str(listProxies[ran]))
+    return proxiReturn
+
+
+
+
+
+
+
+
+
+
 
 def getVotes(url, id):
     listId = []
@@ -35,23 +55,27 @@ def getVotes(url, id):
         firtsLetter = firtsLetter.split()
         listId.append(firtsLetter)
 
-    for i in range(len(firtsLetter)):
-        if (int(firtsLetter[i][0]) == id):
-            print("HELOOO")
-    print(listId)
+
+    for i in listId:
+        #Need validate that
+        if (int(i[0]) == id):
+            print("Holi")
+        #   print("{:i}".format(int(firtsLetter[i + 1][0])))
+        print(i)
+    print("Getting votes...")
 
 
 url = "http://158.69.76.135/level4.php"
 urlProxies = "https://free-proxy-list.net/index.html"
 
-proxies = getProxies(urlProxies)
+
 getVotes(url, int(sys.argv[1]))
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0', 'Referer': 'http://158.69.76.135/level4.php'}
 with requests.session() as client:
-    for i in range(10):
+    for i in range(1):
         lenProxi = 0
-        os.system("unset HTTP_PROXY")
+        os.environ["HTTP_PROXY"] = ""
         key = client.get(url)
         key_dic = key.cookies.get_dict()
         cookies = key.cookies['HoldTheDoor']
@@ -60,11 +84,13 @@ with requests.session() as client:
 			"holdthedoor": "Submit",
 			"key": key_dic['HoldTheDoor']
 		}
-        #os.environ["HTTP_PROXY"] = "http://" + proxies[lenProxi] + ":" + proxies[lenProxi + 1]
+        proxies = getProxies(urlProxies)
+        os.environ["HTTP_PROXY"] = "http://" + proxies[lenProxi] + ":" + proxies[lenProxi + 1]
+        print("POST TO " + proxies[lenProxi] + " PORT: " + proxies[lenProxi + 1])
         #os.environ["HTTP_PROXY"] = "http://112.78.191.35:8080"
-        os.system("export HTTP_PROXY=" + '"' + "110.74.222.213:52733" + '"')
+        #os.system("export HTTP_PROXY=" + '"' + "110.74.222.213:52733" + '"')
         try:
-            client.post(url, data=data_post, headers = headers, cookies={'HoldTheDoor': cookies})
-        except (requests.exceptions.Timeout, requests.exceptions.Timeout, urllib3.exceptions.MaxRetryError) as mg:
+            client.post(url, data=data_post, headers = headers, cookies={'HoldTheDoor': cookies}, timeout=5)
+            print("OK")
+        except (requests.exceptions.Timeout, urllib3.exceptions.MaxRetryError, ConnectionResetError, requests.exceptions.TooManyRedirects, ConnectionRefusedError) as mg:
             print(mg)
-        lenProxi += 2
